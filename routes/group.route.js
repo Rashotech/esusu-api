10b91548b9dbe0f57f03c10c172d39e9101d6724
Rasheed Ayoade <rashotech@gmail.com>
2022-05-19 15:13:38 +0100

    contribution payment endpoint

--- routes/group.route.js
+++ routes/group.route.js
@@ -12,6 +12,7 @@ const router = express.Router();
 router.post('/', auth(), validate(groupValidation.createGroup), groupController.createGroup);
 router.get('/list', groupController.getVisibleGroups);
 router.get('/savings/start/:id', groupAdmin(), validate(groupValidation.startSavingsById), groupController.startSavingsById);
+router.get('/savings/contribute/:id', auth(), validate(groupValidation.contributeMoney), groupController.contributeMoney);
 router.get('/members/:id', groupAdmin(), validate(groupValidation.getMembers), groupController.getMembers);
 router.post('/join/:id', auth(), validate(groupValidation.joinSavingsGroup), groupController.joinSavingsGroup);
 
