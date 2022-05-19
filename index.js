e3c3be1681ddfa04ba13b2de32c496e1a292c206
Rasheed Ayoade <rashotech@gmail.com>
2022-05-19 15:16:56 +0100

    Cron job for Automatic Weekly Saving and Payout Every month

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const { schedule } = require("node-cron");
+const { saveForAll, processPayment } = require("./services/group.service");
+const { isLastDayOfMonth } = require("./utils/date");
+
 const app = require('./app');
 require('./config/database');
 const PORT = process.env.PORT || 3000;
@@ -6,6 +10,32 @@ let server;
 
 server = app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
+});
+
+// Cron Job to automatically help members save every week on saturday
+schedule('0 0 * * SAT', async () => {
+  try {
+    const result = await saveForAll();
+    if (result) {
+      console.log("cron job is complete for now!");
+    }
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
+
+// Cron Job to automatically payout members that are due for payment at the end of every month
+schedule('0 00 16 28-31 * *', async () => {
+  if(isLastDayOfMonth()) {
+    try {
+      const result = await processPayment();
+      if (result) {
+        console.log("cron job is complete for now!");
+      }
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
 });
 
 const exitHandler = () => {
