821f38b8751e94605a6e717121173b0b907f65d7
Rasheed Ayoade <rashotech@gmail.com>
2022-05-19 15:16:24 +0100

    improved saving business logic

--- services/group.service.js
+++ services/group.service.js
@@ -1,11 +1,13 @@
 const httpStatus = require("http-status");
 const uniqid = require("uniqid");
 const Group = require("../models/group.model");
-const { shuffleMembers } = require("../utils/randomGen");
+const UserSavingsData = require("../models/user-savings.model");
+const { generateRandom } = require("../utils/randomGen");
 const ApiError = require("../utils/ApiError");
-const mongoose = require("mongoose");
 
 const createGroup = async (groupBody, userId) => {
+  const { adminToJoin } = groupBody;
+
   const data = {
     ...groupBody,
     uniqueId: uniqid(),
@@ -13,7 +15,12 @@ const createGroup = async (groupBody, userId) => {
   };
 
   const group = await Group.create(data);
-  return group;
+
+  // Add to Join Savings Group Automatically if specified
+  if (adminToJoin && adminToJoin === true) {
+    await joinSavingsGroup(group.uniqueId, userId);
+  }
+  return "Group Created Successfully";
 };
 
 const startSavingsById = async (id) => {
@@ -22,12 +29,39 @@ const startSavingsById = async (id) => {
     throw new ApiError(httpStatus.NOT_FOUND, "Group does not exist");
   }
 
-  if(group.members.length < 1) {
-    throw new ApiError(httpStatus.BAD_REQUEST, "At least one person must be a member before tenure starts");
+  const usersInGroup = await UserSavingsData.find({ group: group._id });
+
+  // Minimum of 3 members are required to start a saving tenure
+  if (usersInGroup.length < 3) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      "At least three person must join before tenure starts"
+    );
+  }
+
+  // Check Capacity Availability
+  if (usersInGroup.length > group.maxCapacity) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      "Group Maximum Capacity Reached"
+    );
   }
 
   try {
-    await GenerateRecipients(id);  
+    // Assign Distribution Order to Users in the Group
+    if (!group.status) {
+      // The Tenure has not yet started
+      let distributionTable = generateRandom(usersInGroup);
+      for (let i = 0; i < usersInGroup.length; i++) {
+        await UserSavingsData.findOneAndUpdate(
+          { group: group._id, _id: distributionTable[i].id },
+          {
+            rank: distributionTable[i].rank,
+            cycle: 4 * usersInGroup.length,
+          }
+        );
+      }
+    }
 
     const updatedGroup = await Group.findOneAndUpdate(
       { uniqueId: id },
@@ -37,7 +71,6 @@ const startSavingsById = async (id) => {
       }
     );
     return updatedGroup;
-    
   } catch (error) {
     throw new ApiError(httpStatus.BAD_REQUEST, error);
   }
@@ -49,20 +82,21 @@ const getVisibleGroups = async () => {
 };
 
 const getMembers = async (id) => {
-  const group = await Group.findOne({ uniqueId: id }).populate({
-    path: "members",
-    select: "email firstName lastName phoneNumber _id",
-  })
-  .populate({
-    path: "nextRecipients",
-    select: "email firstName lastName phoneNumber _id",
-  });
+  const groupMembers = await UserSavingsData.find({ groupId: id })
+    .populate({
+      path: "user",
+      select: "email firstName lastName phoneNumber _id",
+    })
+    .populate({
+      path: "group",
+      select: "name description _id",
+    });
 
-  if (!group) {
+  if (!groupMembers) {
     throw new ApiError(httpStatus.NOT_FOUND, "Group not found");
   }
 
-  return group.members;
+  return groupMembers;
 };
 
 const joinSavingsGroup = async (groupId, userId) => {
@@ -73,20 +107,38 @@ const joinSavingsGroup = async (groupId, userId) => {
     throw new ApiError(httpStatus.NOT_FOUND, "Group not found");
   }
 
-  const userInGroup = await Group.findOne({
-    members: { $elemMatch: { $eq: mongoose.Types.ObjectId(userId) } },
+  const userInGroup = await UserSavingsData.findOne({
+    group: group._id,
+    user: userId,
   });
 
   if (userInGroup) {
     throw new ApiError(httpStatus.BAD_REQUEST, "Already in the Group");
   }
 
-  const option = group.status === false ? { members: userId } : { members: userId, nextRecipients: userId };
+  const unpaidUsers = await UserSavingsData.find({
+    group: group._id,
+    collected: false,
+  });
+
+  const groupMembers = await UserSavingsData.find({
+    group: group._id,
+  });
+
+  const data = {
+    user: userId,
+    group: group._id,
+    rank: group.status === false ? 0 : groupMembers.length + 1,
+    groupId,
+    cycle: unpaidUsers.length * 4,
+  };
 
   try {
-    await Group.updateOne(
-      { uniqueId: groupId },
-      { $addToSet: option }
+    await UserSavingsData.create(data);
+    // Increase all unpaid users payment cycle count
+    await UserSavingsData.updateMany(
+      { collected: false },
+      { $inc: { cycle: 4 } }
     );
     return "Savings Group Joined Successfully";
   } catch (error) {
@@ -95,25 +147,94 @@ const joinSavingsGroup = async (groupId, userId) => {
   }
 };
 
-const GenerateRecipients = async (groupId) => {
+// Contribute Money
+const contributeMoney = async (groupId, userId) => {
+  // After Payment Confirmation from Payment gateway
+  const group = await Group.findOne({ uniqueId: groupId, status: true });
+
+  if (!group) {
+    throw new ApiError(httpStatus.NOT_FOUND, "Group not found");
+  }
+
+  const user = await UserSavingsData.findOne({
+    group: group._id,
+    user: userId,
+  });
+
+  if (user.cycle === 0) {
+    return "All Amount has been paid already";
+  }
+
   try {
-    const group = await Group.findOne({ uniqueId: groupId });
-    let recipientTable = shuffleMembers(group.members);
-    let recipientAdded = [];
-    for (recipient of recipientTable) {
-      recipientAdded = await Group.findOneAndUpdate(
-        {uniqueId: groupId},
-        { $push: { nextRecipients: recipient } },
-        { new: true }
-      );
-    }
-    return recipientAdded;
-  } catch (err) {
-    throw new ApiError(httpStatus.BAD_REQUEST, err);
+    await UserSavingsData.findOneAndUpdate(
+      {
+        group: group._id,
+        user: userId,
+      },
+      {
+        $inc: { amountPaid: group.fixedAmount, cycle: -1 },
+        $addToSet: {
+          payments: {
+            paymentReference: uniqid(),
+            amount: group.fixedAmount,
+            status: "successful",
+          },
+        },
+      }
+    );
+    return "Money Saved Successfully";
+  } catch (error) {
+    console.log(error);
+    throw new ApiError(httpStatus.BAD_REQUEST, error);
   }
 };
 
+// Function to run in a cron job schedule every saturday for Automatic savings
+const saveForAll = async () => {
+  try {
+    const data = await UserSavingsData.find({ collected: false })
+      .populate("group")
+      .populate("user");
+    if (data.length > 0) {
+      data.forEach(async (info) => {
+        await contributeMoney(info.groupId, info.user);
+      });
+      console.log("payment");
+      return true;
+    }
+  } catch (err) {
+    console.log(err);
+    throw new ApiError(httpStatus.NOT_FOUND, err);
+  }
+};
 
+// Process Payment every month for the next inline in every group
+const processPayment = async () => {
+  try {
+    // Get All Groups
+    const data = await UserSavingsData.distinct("groupId", { cycle: 0 });
+    if (data.length > 0) {
+      // Check the groups
+      data.forEach(async (info) => {
+        const group = await UserSavingsData.find({
+          groupId: info,
+          collected: false,
+        }).sort({ rank: "asc" });
+        await UserSavingsData.findOneAndUpdate(
+          { _id: group[0]._id },
+          { collected: true },
+          {
+            new: true,
+          }
+        );
+      });
+      return true;
+    }
+  } catch (err) {
+    console.log(err);
+    throw new ApiError(httpStatus.NOT_FOUND, err);
+  }
+};
 
 module.exports = {
   createGroup,
@@ -121,4 +242,7 @@ module.exports = {
   getVisibleGroups,
   getMembers,
   joinSavingsGroup,
+  contributeMoney,
+  saveForAll,
+  processPayment,
 };
